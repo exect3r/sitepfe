e22fff38a7b8bfc2f7ef8ee33492c6e235ee6caf
Aziz Belaweid <azizbellaweid@hotmail.com>
2021-01-30 19:17:11 +0100

    Fix hardcoded staff, professors and departments

--- src/app/professor/professor.component.ts
+++ src/app/professor/professor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Professor } from 'src/app/models/professors' ; 
 
 @Component({
   selector: 'app-professor',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessorComponent implements OnInit {
 
+  professors : Professor[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.professors = [
+      new Professor(0,'robanna','robanna@insat.com',123456,'info','fulltime'),
+      new Professor(1,'sellaouti','sellaouti@insat.com',123,'info','associate')
+    ]
   }
 
 }

bb48847b3a086bbe7eb75e9397c05b94e5469071
unknown <aymen.letifi@live.com>
2021-02-07 14:23:50 +0100

    kchaou

---
+++ src/app/services/professors/professors.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfessorsService {
+  baseUrl = 'http://localhost:3000/users'
+
+  constructor(private httpclient: HttpClient) { }
+
+  getProfessors(){
+    return this.httpclient.get(this.baseUrl+'/professors');
+  }
+}
